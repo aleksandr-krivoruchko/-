7bfdbdfdb4119d798afa7f5787ff6a50d9e04d9b
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2022-03-07 18:24:41 +0200

    Update

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -15,15 +15,18 @@ let z = 1 + (w/10000*p_b);
 let fog = document.querySelector('.parallax__fog');
 fog.style.transform = `scale(${z})`;
 fog.style.opacity = `${o}`;
+
 //==============дальняя гора=================
 let z_1 = 1 + (w/5000000*p);
 let m_1 = document.querySelector('.parallax__mountain1');
 m_1.style.transform = `scale(${z_1})`;
+
 //==============ближняя правая гора=================
 let hr_2 = w/2000*p_b;
 let z_2 = 1 + (w*0.000005*p_b);
 let m_2 = document.querySelector('.parallax__mountain2');
 m_2.style.transform = `scale(${z_2})`, `translate3d(${hr_2}px,0,0)`;
+
 //==============ближняя левая гора=================
 let hr_3 = w/1500*p_b;
 let z_3 = 1 + (w*0.00001*p_b);
